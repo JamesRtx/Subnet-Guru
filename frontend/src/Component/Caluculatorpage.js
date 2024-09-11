519a185b602abc6e794fc47c1b09c843c94f5e58
[Hakeeb Adekoya] <[adekoyahakeeb@gmail.com]>
2024-09-11 12:09:41 -0400

    Finished adding base material for Teaching page, Now working on algorithm for te calculator page

--- frontend/src/Component/Caluculatorpage.js
+++ frontend/src/Component/Caluculatorpage.js
@@ -1,18 +1,115 @@
-import '../App.css';
+import './Componentcss/Calculator.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavbarBrand from 'react-bootstrap/esm/NavbarBrand';
+import Container from 'react-bootstrap/Container';
+import { NavLink } from "react-router-dom";
+import { useState,useEffect } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+
+
 
 
 const Calculatorpage = () => {
 
+    const [Numberofhost,setNumberofrecenthost] = useState();
+    const [SubnetmaskOptions,setSubnetmaskOptions] = useState([]);
+
+const onChangeNummberofhost =e => {
+    const number = e.target.value
+    setNumberofrecenthost(number)
+}
+const handleSubmitofnumberofhost = () => {
+    const Subnetmask = "0.0.0.0"
+    while (1<= 255){
+
+    }
+console.log(Numberofhost)
+}
 
 
+return (
+<div>
 
-return (<div>
-<h1>Calulor page</h1>
-</div>);
+<h1 className='Headertext'>Scroll Down to Find Your Preferred Calculator</h1>
+<br/>
+<br/>
+{/* Host to subnet */}
+<div className='lineardivider'></div>
+<h2 className='Subsectiontext'>Host-to-Subnet Calculator</h2>
+<p className='paragraphtext'>
+Enter the number of usable hosts you need, and the tool will display the different subnet mask options that fit your requirement, along with the most efficient one.
+</p>
+<div className='ContainerforForm'>
+<Form>
+<Form.Group>
+    <Form.Label>Number Of Host</Form.Label>
+    <div     style={{background:'#32B07A', padding: '10px', borderRadius:'10px'}}    >
+    <Form.Control
+    style={{border:'2px solid green'}}
+            type="text"
+            placeholder="Example: 28"
+             value={Numberofhost}
+             onChange={onChangeNummberofhost}
+             />
+             </div>
+</Form.Group>
+<Button variant="primary" onClick={handleSubmitofnumberofhost}>
+             Submit
+           </Button>
+</Form>
+
+
+{Numberofhost != null ? 
+( 
+<div style={{}}>
+    <h1> For {Numberofhost}</h1>
+</div> 
+
+) :
+ (
+ <div>
+    
+    
+    </div>)    
+ 
+ }
+
+
+</div>
+{/* <div className='lineardivider'></div>
+<h2 className='Subsectiontext'>Host Range & Usability Checker</h2>
+<p className='paragraphtext'>
+To determine the minimum number of hosts and the range of usable IP addresses, please click on the form and enter the IP address and subnet mask (e.g., /24). The tool will then display the minimum number of usable hosts and the range of IP addresses available within the specified subnet.</p>
+ */}
+
+{/* <div className='lineardivider'></div>
+<h2 className='Subsectiontext'>Broadcast Address Calculator</h2>
+<p className='paragraphtext'>
+To find the broadcast address for a given subnet, click on the form and enter the IP address and subnet mask (e.g., /24). The tool will then calculate and display the broadcast address for the specified subnet.
+
+</p> */}
+
+{/* <div className='lineardivider'></div>
+<h2 className='Subsectiontext'>CIDR Notation Finder</h2>
+<p className='paragraphtext'>
+To determine the subnet size in CIDR notation, click on the form and enter the IP address and subnet mask (e.g., /24 or /28). The tool will then display the subnet size in CIDR notation, helping you understand the network's capacity and range.
+</p> */}
+
+<div className='lineardivider'></div>
+<h2 className='Subsectiontext'>Same Subnet Checker/Same Network Identifier</h2>
+<p className='paragraphtext'>
+To determine if two IP addresses are in the same subnet, click on the form and enter the IP address, subnet mask, and the second IP address. The tool will then check and display whether both IP addresses belong to the same subnet.
+
+
+</p>
+
+</div>
+
+);
 
 
 }
--- frontend/src/Component/TeachingPage.js
+++ frontend/src/Component/TeachingPage.js
@@ -6,6 +6,11 @@ import NavbarBrand from 'react-bootstrap/esm/NavbarBrand';
 import Container from 'react-bootstrap/Container';
 import { NavLink } from "react-router-dom";
 
+import Subnettingexample1 from  "../images/Subnetingexample1.png"
+import Subnettingexample2 from  "../images/Subnetingexample2.png"
+import Subnettingexample3 from  "../images/Subnettingexample3.png"
+import Subnettingexample4 from  "../images/Subnettingexample4.png"
+
 const Teachingpage =() => {
 
 
@@ -43,10 +48,107 @@ const Teachingpage =() => {
 </div>
 <h2 className='Subsectiontext'>Basic understanding of Subnet mask and Their Purpose</h2>
 <p className='paragraphtext'>
-    sd
+A subnet mask is a crucial component in networking that helps define which part of an IP address is used to identify the network and which part is used for individual devices (hosts) within that network.
+<br/>
+<br/>
+A subnet mask is a 32-bit number, similar to an IP address, written in dotted decimal notation. The subnet mask is composed of a sequence of 1s and 0s, with values of 1s representing the network component of the IP address. "0s" denote the host component of the IP address.
+<br/>
+To illustrate: 
+    <div className='container'>
+    <img src={Subnettingexample1} alt="Nature" className="responsive" />
+    <br/>
+    <br/>
+    <h1 style={{textAlign:"center"}}> Binary representation</h1>
+    <img src={Subnettingexample2} alt="Nature" className="responsive" />
+
+    </div>
+    <br/>
+    Note: A single octet value of a subnet mask is typically either 255 or 0. However, with the use of CIDR (Classless Inter-Domain Routing), more flexible values, like 192 or 128, can also be used.
+    
     </p>
 
 
+    <div className='lineardivider'>
+    </div>
+    <h2 className='Subsectiontext'>Classful IP Addressing</h2>
+        <p className='paragraphtext'>
+        Classful addressing was the original method used to allocate IP addresses and organize networks. This system divided the IP address space into predefined classes with fixed boundaries. While these classes simplified network management, they had significant limitations, especially as the internet expanded and the demand for IP addresses increased. Classful addressing classified IP addresses into five classes (A, B, C, D, and E), each designed to serve different types of networks, from large global enterprises to small local networks.
+<br/>
+    <h3>Class A :</h3>
+            <ul style={{margin:"10px" ,fontSize:'1.6vw'}}>
+                <li>Range of the First Octet: From 1 to 127</li>
+                <li>Address Range: 1.0.0.0 to 126.0.0.0</li>
+                <li>Purpose: Designed for very large networks, such as major organizations, large corporations, or even entire countries. Class A networks can accommodate a vast number of devices.</li>
+                <li>Subnet Mask: 255.0.0.0</li>
+            </ul> 
+        
+    <h3>Class B :</h3>
+    <ul style={{margin:"10px" ,fontSize:'1.6vw'}}>
+        <li>Range of the First Octet: From 128 to 191</li>
+        <li>Address Range: 128.0.0.0 to 191.255.0.0</li>
+        <li>Purpose: Suitable for medium-sized networks, such as universities, large businesses, or regional networks. Class B networks offer a balance between the number of available networks and the number of hosts per network.        </li>
+        <li>Subnet Mask: 255.255.0.0</li>
+    </ul> 
+    <h3>Class C :</h3>
+    <ul style={{margin:"10px" ,fontSize:'1.6vw'}}>
+        <li>Range of the First Octet: From 192 to 223</li>
+        <li>Address Range: 192.0.0.0 to 223.255.255.0</li>
+        <li>Purpose: Intended for smaller networks, such as small businesses, local area networks (LANs), or home networks. Class C networks provide a sufficient number of IP addresses for smaller organizations.        </li>
+        <li>Subnet Mask: 255.255.255.0</li>
+    </ul> 
+
+    <h3>Class D :</h3>
+    <ul style={{margin:"10px" ,fontSize:'1.6vw'}}>
+        <li>Range of the First Octet: From 224 to 239</li>
+        <li>Address Range: 224.0.0.0 to 239.255.255.255</li>
+        <li>Purpose: Reserved for multicasting. Class D addresses are used to send data to multiple devices simultaneously rather than to individual hosts. This class is not used for regular network communication.        </li>
+    </ul> 
+    
+    <h3>Class E :</h3>
+    <ul style={{margin:"10px" ,fontSize:'1.6vw'}}>
+        <li>Range of the First Octet: From 240 to 255</li>
+        <li>Address Range: 240.0.0.0 to 255.255.255.255</li>
+        <li>Purpose: Reserved for experimental purposes and future use. Class E addresses are not used in standard network communication and are reserved for research and experimental applications.
+        </li>
+    </ul> 
+
+        </p>
+<div className='lineardivider'>
+    </div>
+    <h2 className='Subsectiontext'>Subnetting with Classless Inter-Domain Routing (CIDR)</h2>
+    <p className='paragraphtext'>
+
+
+
+    In the early days of the internet, a system called "classful ip addressing" was used to manage IP addresses. It aimed to simplify network administration by assigning blocks of addresses based on network size. While initially helpful, classful addressing struggled to keep pace with the internet's explosive growth. The limitations became clear - the system couldn't efficiently allocate addresses for both very large and very small networks. This mismatch between address needs and available blocks led to the development of a more flexible system – Classless Inter-Domain Routing (CIDR).
+    <br/> <br/>
+    CIDR was introduced to address the inefficiencies of classful addressing by providing a more granular and flexible method for IP address allocation. Unlike classful addressing, CIDR does not rely on fixed address classes. Instead, it allows network administrators to allocate IP addresses based on the specific needs of their networks, resulting in more efficient use of the available IP address space.
+    <br/>
+    In traditional subnetting, subnet masks are expressed as sequences of 255s and 0s, such as 255.255.255.0 or 255.255.0.0. However, with CIDR , subnet masks can include values other than just 255 or 0, such as 192, allowing for more flexible and efficient subnetting.
+    <br/>
+    To illustrate:
+    <br/>
+     Traditional Subnet Masks :         
+    <br/>                                     
+                                     <p style={{textAlign:'center'}}>    255. 255 .255 .0 or /24 (wack 24)</p>
+                                     Subnet Masks Using CIDR :            
+    <br/>                                     
+                                     <p style={{textAlign:'center'}}>   255. 255 .192 .0    or /25 (wack 25)</p>
+                                     <br/>   
+                                     When subnetting with CIDR, we use CIDR notation (e.g., /24) to simplify and understand subnetting. This notation replaces the need to convert binary subnet masks to determine the network and host portions of an IP address. By using CIDR notation, you can quickly identify how many bits are allocated for the network portion and how many are available for host addresses, streamlining the subnetting process.
+<br/>
+To illustrate :
+
+<div className='container'>
+    <img src={Subnettingexample3} alt="Nature" className="responsive" />
+    <br/>
+    <br/>
+    <img src={Subnettingexample4} alt="Nature" className="responsive" />
+
+    </div>
+
+                                          
+    </p>
 
 
 
