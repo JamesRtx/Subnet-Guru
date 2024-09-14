07cdc4334f6040a8d7383b4d8711d825f90018e3
[Hakeeb Adekoya] <[adekoyahakeeb@gmail.com]>
2024-09-14 10:38:09 -0400

    Finshed the Quizzpage,Now working Faq page

--- frontend/src/Component/PracticeQuizzes_Exercises.js
+++ frontend/src/Component/PracticeQuizzes_Exercises.js
@@ -1,4 +1,4 @@
-import './Componentcss/Calculator.css';
+import './Componentcss/PracticeQuizzes_Exercises.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
@@ -9,11 +9,59 @@ import { useState,useEffect } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
-
+import QuestionsAndAnswer from './Questionandanswer';
 const PracticeQuizzes_Exercises = () => {
 
+const [ShowAnwser,setShowAnwser] =  useState(false)
+
+const OnchangeShowAnwser = () => {   
+    setShowAnwser(true)
+}
+
+    return(<div>
+        <h1 className='Headertext'>Take a Quick Quiz to Test Your Subnetting Skills!</h1>
+        
+        <br/>
+        <br/>
+        <div className='lineardivider'></div>
+        {QuestionsAndAnswer.map(Quizz => {
+            return (
+            
 
 
-    return(<div><h1>This is the Practice Quizzes/Exercises page</h1></div>)
+
+
+<div className='ContainerforQuestion'> 
+    {Quizz.question}
+    {Quizz.option.map(options =>{       
+        return (
+        <div>
+                    <input
+                            type="radio"
+                            name="option"
+                           
+                            className="form-check-input"
+                        />
+                        <label> {options}</label>
+        </div>
+    )      })}
+    
+
+    <br/>
+
+
+           {ShowAnwser ? (<div>{Quizz.answer}</div>):(<div></div>)}  
+
+            </div>     
+
+            )
+        })}
+        
+
+        <Button  className='Submitbutton' variant="primary"  onClick={OnchangeShowAnwser} >
+             Answer?
+           </Button>
+        
+        </div>)
 }
 export default PracticeQuizzes_Exercises
---
+++ frontend/src/Component/Questionandanswer.js
@@ -0,0 +1,59 @@
+
+
+const QuestionsAndAnswer = [
+{
+    question: "1. What is the purpose of a subnet mask in networking?",
+    option: ["A) To identify the network and host portions of an IP address"," B) To determine the broadcast address","C) To assign IP addresses automatically","D) To translate IP addresses into MAC addresses"],
+    answer: "A) To identify the network and host portions of an IP address"
+    
+}
+,
+
+{
+    question: "2. If an IP address is 192.168.1.25 with a subnet mask of 255.255.255.0, what is the network ID?",
+    option: ["A) 192.168.1.0","B) 192.168.1.25","C) 192.168.1.255","D) 192.168.0.0"],
+    answer: "A) 192.168.1.0"
+}
+,
+
+{
+    question: "3. What does CIDR stand for, and why was it introduced?",
+    option: ["A) Classless Inter-Domain Routing; to replace classful addressing with more efficient IP address allocation","B) Circuit Identification Routing; to improve communication between subnets","C) Code Identification Routing; to handle larger networks","D) Classful Identification Routing; to maintain rigid IP address class structures"],
+    answer: "A) Classless Inter-Domain Routing; to replace classful addressing with more efficient IP address allocation"
+}
+,
+{
+    question: "4. Given a subnet mask of 255.255.255.240, what is the corresponding CIDR notation?",
+    option: ["A) /28","B) /24","C) /32","D) /29"],
+    answer: "A) /28"
+}
+,
+{
+    question: "5. Which class of IP address has a default subnet mask of 255.255.0.0?",
+    option: ["A) Class A","B) Class B","C) Class C","D) Class D"],
+    answer: "A) /28"
+
+}
+,
+{
+    question: "6. If two IP addresses have the same network ID but different host IDs, are they in the same subnet?",
+    option: ["A) Yes","B) No"],
+    answer: "A) Yes"
+
+}
+,
+{
+    question: "7. What is the maximum number of usable hosts in a subnet with CIDR notation /29?",
+    option: [" A) 8","B) 4","C) 6","D) 2"],
+    answer: "C) 6"
+
+}
+,
+{
+    question: "8. In the IP address 172.16.0.75/16, what part of the address represents the host portion?",
+    option: ["A) 172.16","B) 0.75","C) 172","D) 16"],
+    answer: "A) 172.16"
+}
+]
+
+export default QuestionsAndAnswer
